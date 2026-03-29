6bf5321c4f10ccc6966bae2bcb23430b50540559
Chaojie Wang <2387670567@qq.com>
2026-03-29 18:33:42 +0800

    style: force tailwind rendering fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css'  // 确保文件名和你第一步的文件名一致
